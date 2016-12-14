2de194704dceeb2d79f49032eeb3380f25d30d1a
jamesv84 <jamesvelez84@yahoo.com>
2016-12-14 11:23:19 -0800

    Update bluemix-settings.js

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -24,7 +24,7 @@ var VCAP_APPLICATION = JSON.parse(process.env.VCAP_APPLICATION);
 var VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES);
 
 var settings = module.exports = {
-    httpNodeCors: { origin: “*”, methods: ['GET','PUT','POST','DELETE'] },
+    httpNodeCors: { origin: "*", methods: ['GET','PUT','POST','DELETE'] },
     uiPort: process.env.PORT || 1880,
     mqttReconnectTime: 15000,
     serialReconnectTime: 15000,
